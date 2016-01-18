1918f6b86c8cd3797175f6ea5be5637a45f6fad6
Harshul jain <harshulrobo@gmail.com>
2016-01-18 13:08:36 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^klop/', include('medwebapp.urls')),
+    url(r'^halc/', include('medwebapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
